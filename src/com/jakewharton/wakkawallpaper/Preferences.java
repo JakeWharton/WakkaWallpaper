4fa22f4377679e9e5180db009f5d65ce3a64c425
Jake Wharton <jakewharton@gmail.com>
2010-09-06 18:43:38 -0400

    Actually delete preferences.

--- src/com/jakewharton/wakkawallpaper/Preferences.java
+++ src/com/jakewharton/wakkawallpaper/Preferences.java
@@ -689,35 +689,35 @@ public class Preferences extends PreferenceActivity implements SharedPreferences
 	    final SharedPreferences.Editor editor = Preferences.this.getPreferenceManager().getSharedPreferences().edit();
 		
 		//fps
-		editor.putInt(resources.getString(R.string.settings_display_fps_key), resources.getInteger(R.integer.display_fps_default));
+		editor.remove(resources.getString(R.string.settings_display_fps_key));
 		//juggerdot blink
-		editor.putInt(resources.getString(R.string.settings_display_juggerdotblink_key), resources.getInteger(R.integer.display_juggerdotblink_default));
+		editor.remove(resources.getString(R.string.settings_display_juggerdotblink_key));
 		//show hud
-		editor.putBoolean(resources.getString(R.string.settings_display_showhud_key), resources.getBoolean(R.bool.display_showhud_default));
+		editor.remove(resources.getString(R.string.settings_display_showhud_key));
 		//hud size
-		editor.putInt(resources.getString(R.string.settings_display_hudsize_key), resources.getInteger(R.integer.display_hudsize_default));
+		editor.remove(resources.getString(R.string.settings_display_hudsize_key));
 		//hud offset
-		editor.putInt(resources.getString(R.string.settings_display_hudoffset_key), resources.getInteger(R.integer.display_hudoffset_default));
+		editor.remove(resources.getString(R.string.settings_display_hudoffset_key));
 		//show walls
-		editor.putBoolean(resources.getString(R.string.settings_display_showwalls_key), resources.getBoolean(R.bool.display_showwalls_default));
+		editor.remove(resources.getString(R.string.settings_display_showwalls_key)));
 		//icon rows
-		editor.putInt(resources.getString(R.string.settings_display_iconrows_key), resources.getInteger(R.integer.display_iconrows_default));
+		editor.remove(resources.getString(R.string.settings_display_iconrows_key));
 		//icon cols
-		editor.putInt(resources.getString(R.string.settings_display_iconcols_key), resources.getInteger(R.integer.display_iconcols_default));
+		editor.remove(resources.getString(R.string.settings_display_iconcols_key));
 		//icon row spacing
-		editor.putInt(resources.getString(R.string.settings_display_rowspacing_key), resources.getInteger(R.integer.display_rowspacing_default));
+		editor.remove(resources.getString(R.string.settings_display_rowspacing_key));
 		//icon col spacing
-		editor.putInt(resources.getString(R.string.settings_display_colspacing_key), resources.getInteger(R.integer.display_colspacing_default));
+		editor.remove(resources.getString(R.string.settings_display_colspacing_key));
 		//widget locations
-		editor.putString(resources.getString(R.string.settings_display_widgetlocations_key), resources.getString(R.string.display_widgetlocations_default));
+		editor.remove(resources.getString(R.string.settings_display_widgetlocations_key));
 		//padding top
-		editor.putInt(resources.getString(R.string.settings_display_padding_top_key), resources.getInteger(R.integer.display_padding_top_default));
+		editor.remove(resources.getString(R.string.settings_display_padding_top_key));
 		//padding bottom
-		editor.putInt(resources.getString(R.string.settings_display_padding_bottom_key), resources.getInteger(R.integer.display_padding_bottom_default));
+		editor.remove(resources.getString(R.string.settings_display_padding_bottom_key));
 		//padding left
-		editor.putInt(resources.getString(R.string.settings_display_padding_left_key), resources.getInteger(R.integer.display_padding_left_default));
+		editor.remove(resources.getString(R.string.settings_display_padding_left_key));
 		//padding right
-		editor.putInt(resources.getString(R.string.settings_display_padding_right_key), resources.getInteger(R.integer.display_padding_right_default));
+		editor.remove(resources.getString(R.string.settings_display_padding_right_key));
 	
 		editor.commit();
 	}
@@ -730,43 +730,43 @@ public class Preferences extends PreferenceActivity implements SharedPreferences
 		final SharedPreferences.Editor editor = Preferences.this.getPreferenceManager().getSharedPreferences().edit();
 		
 		//mode
-		editor.putInt(resources.getString(R.string.settings_game_mode_key), resources.getInteger(R.integer.game_mode_default));
+		editor.remove(resources.getString(R.string.settings_game_mode_key));
 		//endless dot threshold
-		editor.putInt(resources.getString(R.string.settings_game_endlessdotregen_key), resources.getInteger(R.integer.game_endlessdotregen_default));
+		editor.remove(resources.getString(R.string.settings_game_endlessdotregen_key));
 		//endless juggerdot threshold
-		editor.putInt(resources.getString(R.string.settings_game_endlessjuggerdotregen_key), resources.getInteger(R.integer.game_endlessjuggerdotregen_default));
+		editor.remove(resources.getString(R.string.settings_game_endlessjuggerdotregen_key));
 		//display kill screen
-		editor.putBoolean(resources.getString(R.string.settings_game_killscreen_key), resources.getBoolean(R.bool.game_killscreen_default));
+		editor.remove(resources.getString(R.string.settings_game_killscreen_key));
 		//wrapping mode
-		editor.putInt(resources.getString(R.string.settings_game_wrappingmode_key), resources.getInteger(R.integer.game_wrappingmode_default));
+		editor.remove(resources.getString(R.string.settings_game_wrappingmode_key));
 		//the man wrapping
-		editor.putBoolean(resources.getString(R.string.settings_game_wrappingtheman_key), resources.getBoolean(R.bool.game_wrappingtheman_default));
+		editor.remove(resources.getString(R.string.settings_game_wrappingtheman_key));
 		//ghost wrapping
-		editor.putBoolean(resources.getString(R.string.settings_game_wrappingghosts_key), resources.getBoolean(R.bool.game_wrappingghosts_default));
+		editor.remove(resources.getString(R.string.settings_game_wrappingghosts_key));
 		//the man mode
-		editor.putInt(resources.getString(R.string.settings_game_themanmode_key), resources.getInteger(R.integer.game_themanmode_default));
+		editor.remove(resources.getString(R.string.settings_game_themanmode_key));
 		//user controllable
-		editor.putBoolean(resources.getString(R.string.settings_game_usercontrol_key), resources.getBoolean(R.bool.game_usercontrol_default));
+		editor.remove(resources.getString(R.string.settings_game_usercontrol_key));
 		//ghost mode
-		editor.putInt(resources.getString(R.string.settings_game_ghostmode_key), resources.getInteger(R.integer.game_ghostmode_default));
+		editor.remove(resources.getString(R.string.settings_game_ghostmode_key));
 		//ghosts are deadly
-		editor.putBoolean(resources.getString(R.string.settings_game_deadlyghosts_key), resources.getBoolean(R.bool.game_deadlyghosts_default));
+		editor.remove(resources.getString(R.string.settings_game_deadlyghosts_key));
 		//ghost count
-		editor.putInt(resources.getString(R.string.settings_game_ghostcount_key), resources.getInteger(R.integer.game_ghostcount_default));
+		editor.remove(resources.getString(R.string.settings_game_ghostcount_key));
 		//bonus allowed
-		editor.putBoolean(resources.getString(R.string.settings_game_bonuslife_key), resources.getBoolean(R.bool.game_bonuslife_default));
+		editor.remove(resources.getString(R.string.settings_game_bonuslife_key));
 		//bonus threshold
-		editor.putInt(resources.getString(R.string.settings_game_bonuslifethreshold_key), resources.getInteger(R.integer.game_bonuslifethreshold_default));
+		editor.remove(resources.getString(R.string.settings_game_bonuslifethreshold_key));
 		//fruit enabled
-		editor.putBoolean(resources.getString(R.string.settings_game_fruitenabled_key), resources.getBoolean(R.bool.game_fruitenabled_default));
+		editor.remove(resources.getString(R.string.settings_game_fruitenabled_key));
 		//fruit one threshold
-		editor.putInt(resources.getString(R.string.settings_game_fruitonethreshold_key), resources.getInteger(R.integer.game_fruitonethreshold_default));
+		editor.remove(resources.getString(R.string.settings_game_fruitonethreshold_key));
 		//fruit two threshold
-		editor.putInt(resources.getString(R.string.settings_game_fruittwothreshold_key), resources.getInteger(R.integer.game_fruittwothreshold_default));
+		editor.remove(resources.getString(R.string.settings_game_fruittwothreshold_key));
 		//fruit visible lower
-		editor.putInt(resources.getString(R.string.settings_game_fruitvisiblelower_key), resources.getInteger(R.integer.game_fruitvisiblelower_default));
+		editor.remove(resources.getString(R.string.settings_game_fruitvisiblelower_key));
 		//fruit visible upper
-		editor.putInt(resources.getString(R.string.settings_game_fruitvisibleupper_key), resources.getInteger(R.integer.game_fruitvisibleupper_default));
+		editor.remove(resources.getString(R.string.settings_game_fruitvisibleupper_key));
 
 		editor.commit();
 	}
@@ -779,51 +779,51 @@ public class Preferences extends PreferenceActivity implements SharedPreferences
 		final SharedPreferences.Editor editor = Preferences.this.getPreferenceManager().getSharedPreferences().edit();
 		
 		//background
-		editor.putInt(resources.getString(R.string.settings_color_game_background_key), resources.getInteger(R.integer.color_game_background_default));
+		editor.remove(resources.getString(R.string.settings_color_game_background_key));
 		//walls
-		editor.putInt(resources.getString(R.string.settings_color_game_walls_key), resources.getInteger(R.integer.color_game_walls_default));
+		editor.remove(resources.getString(R.string.settings_color_game_walls_key));
 		//background image
-		editor.putString(resources.getString(R.string.settings_color_game_bgimage_key), null);
+		editor.remove(resources.getString(R.string.settings_color_game_bgimage_key));
 		//background opacity
-		editor.putInt(resources.getString(R.string.settings_color_game_bgopacity_key), resources.getInteger(R.integer.color_game_bgopacity_default));
+		editor.remove(resources.getString(R.string.settings_color_game_bgopacity_key));
 		//dots
-		editor.putInt(resources.getString(R.string.settings_color_game_dot_key), resources.getInteger(R.integer.color_game_dot_default));
+		editor.remove(resources.getString(R.string.settings_color_game_dot_key));
 		//juggerdots
-		editor.putInt(resources.getString(R.string.settings_color_game_juggerdot_key), resources.getInteger(R.integer.color_game_juggerdot_default));
+		editor.remove(resources.getString(R.string.settings_color_game_juggerdot_key));
 		//hud foreground
-		editor.putInt(resources.getString(R.string.settings_color_game_hudfg_key), resources.getInteger(R.integer.color_game_hudfg_default));
+		editor.remove(resources.getString(R.string.settings_color_game_hudfg_key));
 		//hud background
-		editor.putInt(resources.getString(R.string.settings_color_game_hudbg_key), resources.getInteger(R.integer.color_game_hudbg_default));
+		editor.remove(resources.getString(R.string.settings_color_game_hudbg_key));
 		//ready color
-		editor.putInt(resources.getString(R.string.settings_color_game_ready_key), resources.getInteger(R.integer.color_game_ready_default));
+		editor.remove(resources.getString(R.string.settings_color_game_ready_key));
 		//game over color
-		editor.putInt(resources.getString(R.string.settings_color_game_gameover_key), resources.getInteger(R.integer.color_game_gameover_default));
+		editor.remove(resources.getString(R.string.settings_color_game_gameover_key));
 		//"The Man"
-		editor.putInt(resources.getString(R.string.settings_color_theman_key), resources.getInteger(R.integer.color_theman_default));
+		editor.remove(resources.getString(R.string.settings_color_theman_key));
 		//eye background
-		editor.putInt(resources.getString(R.string.settings_color_ghost_eyebg_key), resources.getInteger(R.integer.color_ghost_eyebg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_eyebg_key));
 		//eye foreground
-		editor.putInt(resources.getString(R.string.settings_color_ghost_eyefg_key), resources.getInteger(R.integer.color_ghost_eyefg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_eyefg_key));
 		//scared body
-		editor.putInt(resources.getString(R.string.settings_color_ghost_scaredbg_key), resources.getInteger(R.integer.color_ghost_scaredbg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_scaredbg_key));
 		//scared eyes
-		editor.putInt(resources.getString(R.string.settings_color_ghost_scaredfg_key), resources.getInteger(R.integer.color_ghost_scaredfg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_scaredfg_key));
 		//scared blinking body
-		editor.putInt(resources.getString(R.string.settings_color_ghost_scaredblinkbg_key), resources.getInteger(R.integer.color_ghost_scaredblinkbg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_scaredblinkbg_key));
 		//scared blinking eyes
-		editor.putInt(resources.getString(R.string.settings_color_ghost_scaredblinkfg_key), resources.getInteger(R.integer.color_ghost_scaredblinkfg_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_scaredblinkfg_key));
 		//blinky
-		editor.putInt(resources.getString(R.string.settings_color_ghost_blinky_key), resources.getInteger(R.integer.color_ghost_blinky_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_blinky_key));
 		//pinky
-		editor.putInt(resources.getString(R.string.settings_color_ghost_pinky_key), resources.getInteger(R.integer.color_ghost_pinky_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_pinky_key));
 		//inky
-		editor.putInt(resources.getString(R.string.settings_color_ghost_inky_key), resources.getInteger(R.integer.color_ghost_inky_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_inky_key));
 		//clyde
-		editor.putInt(resources.getString(R.string.settings_color_ghost_clyde_key), resources.getInteger(R.integer.color_ghost_clyde_default));
+		editor.remove(resources.getString(R.string.settings_color_ghost_clyde_key));
 		//entity style
-		editor.putInt(resources.getString(R.string.settings_color_entitystyle_key), resources.getInteger(R.integer.color_entitystyle_default));
+		editor.remove(resources.getString(R.string.settings_color_entitystyle_key));
 		//dot style
-		editor.putInt(resources.getString(R.string.settings_color_dotstyle_key), resources.getInteger(R.integer.color_dotstyle_default));
+		editor.remove(resources.getString(R.string.settings_color_dotstyle_key));
 		
 		editor.commit();
 	}
